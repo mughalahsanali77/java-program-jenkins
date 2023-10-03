8069d7b2f5d773b2ac92e49d96c5201232e9ebb3
Ahsan Ali Mughal <mughalahsanali77@gmail.com>
2023-10-03 10:08:48 +0500

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This Is Java Projram");
+        System.out.println("Output of Java program ");
+        System.out.println(new Date());
+    }
+}
